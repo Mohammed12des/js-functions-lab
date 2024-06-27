8bd495aa046afe4e90d89153e439712372c10a83
Mohammed12des <hamood12des@gmail.com>
2024-06-27 16:41:17 +0300

    done

--- script.js
+++ script.js
@@ -143,3 +143,59 @@ const calculateTip=(billamount , tippercentage)=>{
 
 
 console.log('Exercise 7 Result:', calculateTip(50, 20));
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature=(temperature , scale )=>{
+  if (scale === 'C') {
+
+      return (temperature * 9/5) + 32;
+  } else if (scale === 'F') {
+      
+      return (temperature - 32) * 5/9;
+  } else {
+      return 'Invalid ';
+  }
+}
+
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+const basicCalculator=(a,b,c)=>{
+  if(c==='add')
+      a+b
+  if(c==='subtract')
+      a-b
+  if(c==='multiply')
+      a*b
+  if(c==='divide')
+      a/b
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
